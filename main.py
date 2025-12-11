bb4e52ba99020b4cfa0bf483e9df3378c7079d32
Sasho <aleksandarkolev@Aleksandars-MacBook-Pro.local>
2025-12-11 16:47:08 +0200

    updated

--- main.py
+++ main.py
@@ -26,11 +26,32 @@ while True:
         case "edit":
             number = int(input("What is the number of the todo to edit: "))
             number = number - 1
+
+            with open('todos.txt', 'r')as file:
+                todos = file.readlines()
+
             new_todo = input("Edit your todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case "complete":
             number = int(input("What is the number of the completed todo: "))
-            todos.pop(number - 1)
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+
         case "exit":
             break
         case whatever:
